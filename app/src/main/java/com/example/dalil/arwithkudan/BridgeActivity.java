9a88ee22ed809967105caa1f3035721bd2544c58
dalilaisan <dalila.isan@gmail.com>
2018-05-14 11:46:58 +0200

    arbi track working (switching target with tracked node), along with marker tracking

--- app/src/main/java/com/example/dalil/arwithkudan/BridgeActivity.java
+++ app/src/main/java/com/example/dalil/arwithkudan/BridgeActivity.java
@@ -1,6 +1,10 @@
 package com.example.dalil.arwithkudan;
 
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 import eu.kudan.kudan.ARAPIKey;
 import eu.kudan.kudan.ARActivity;
@@ -10,23 +14,27 @@ import eu.kudan.kudan.ARImageNode;
 import eu.kudan.kudan.ARImageTrackable;
 import eu.kudan.kudan.ARImageTracker;
 
-public class BridgeActivity extends ARActivity {
+public class BridgeActivity extends ARActivity implements GestureDetector.OnGestureListener {
 
+    private static final String TAG = "BridgeActivity";
+    private GestureDetectorCompat gestureDetect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bridge);
 
-
-
         ARAPIKey key = ARAPIKey.getInstance();
         key.setAPIKey("agWZcpYLYjBxCbWf2qZx6k+PWISqeGtFCqKaZwYtwS+kdn1HKiQAmsJ55STRBe9BqCw3VwG6qL+ESI5ntTF/iV/uekLG3PCokaUE0/uTzqhaYlxRdmuNBIduzBCjq3mV2na+gy3ffHH9Ipc7eIN0geTj3p+ppsmK0U399iGmN38ndIh6k2y16cByWIecMSU3yw3Ztw7gHRqf83hVhZ5T2ACGK4SNkQhhdKp+CTaR5W3amYCJBgwumqFqNFyI9UniuMk70T/cQObRQum2U51OjjbMfmEAwIBt8Q8jD2yACzye6K4/1O4pZhbGEbiDeLrAfxqMwBAe5o6vnYIilGNnpDhfi3wOHhRaqtLOVvB58GUIFTnAPvmYFVnLWRJmCUZ9FJNDyX3ALCl/alFEWh+A/a6NFjcwLGKI9drPuGG4ONFg4p0l+p3b9DZoLzszlmWAflI/UFzQa++kQn3/sclO9i0vPnpi0LWoABm5vGswLVAIX/0k6384GXxfkADI6fjGtf62XJ5ImaVDiiREa9mabWEQGoifghQG1sGNDYgBIYEpiaLsVzOfTALpe20Q7kFCMjedJImQhhuLtEK1BXfXJEed1QqUOsG9IeKxKk28GbOtOF9w3yrSF3gnJslzZxF2kEF3C6ckog8byagS+4p37FJmbpPsiKNH1Qm0LuouGcQ=");
+
+        // Create gesture recogniser to start and stop arbitrack
+        gestureDetect = new GestureDetectorCompat(this,this);
         }
 
     @Override
-    public void setup()
-    {
+    public void setup() {
+        Log.d(TAG, "setup: called");
         super.setup();
 
         // Initialise the image trackable and load the image.
@@ -42,8 +50,8 @@ public class BridgeActivity extends ARActivity {
         imageTracker.addTrackable(imageTrackable);
 
 
-
         // Initialise the image node with our image
+        Log.d(TAG, "setup: setting up image node");
         ARImageNode imageNode = new ARImageNode("Arrow.png");
 
         //imageNode.rotateByDegrees(0.0f, 1.0f, 0.0f, 0.0f);
@@ -51,24 +59,26 @@ public class BridgeActivity extends ARActivity {
 
         // Add the image node as a child of the trackable's world
         imageTrackable.getWorld().addChild(imageNode);
-       // imageNode.setName("Cow");
+        // imageNode.setName("Cow");
 
         /////////////////////////TRACK (at the end of setup)
 
         // Initialise ArbiTrack.
+        Log.d(TAG, "setup: artbitrach initialized");
         ARArbiTrack arbiTrack = ARArbiTrack.getInstance();
         arbiTrack.initialise();
 
         //Add the activity as an ArbiTrack delegate
 
-      //  arbiManager.addListener(this);
+        //  arbiManager.addListener(this);
 
         // Initialise gyro placement.
+        Log.d(TAG, "setup: gyro placement initialized");
         ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
         gyroPlaceManager.initialise();
 
 
-        /////ADDING THE TAFGET NODE
+        /////ADDING THE TARGET NODE
 
         // Create a node to be used as the target.
         ARImageNode targetNode = new ARImageNode("Cow Target.png");
@@ -86,8 +96,76 @@ public class BridgeActivity extends ARActivity {
         arbiTrack.setTargetNode(targetNode);
 
 
+        //!!!!!SETTING UP THE TRACKING NODE:
+
+        // Create a node to be tracked.
+        ARImageNode trackingNode = new ARImageNode("Cow Tracking.png");
+
+        // Rotate the node to ensure it is displayed correctly.
+        trackingNode.rotateByDegrees(90.0f, 1.0f, 0.0f, 0.0f);
+        trackingNode.rotateByDegrees(180.0f, 0.0f, 1.0f, 0.0f);
+
+        // Add the node as a child of the ArbiTracker's world.
+        arbiTrack.getWorld().addChild(trackingNode);
+
+    }
+
+        ///!!!!!!TOUCH INPUT:
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event){
+            Log.d(TAG, "onTouchEvent: touch event detected!");
+            gestureDetect.onTouchEvent(event);
+            return super.onTouchEvent(event);
+        }
 
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
 
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.d(TAG, "onSingleTapUp: tap up detected!");
+        ARArbiTrack arbiTrack = ARArbiTrack.getInstance();
+
+        // If arbitrack is tracking, stop tracking so that its world is no longer rendered, and make the target node visible.
+        if (arbiTrack.getIsTracking())
+        {
+            arbiTrack.stop();
+            Log.d(TAG, "onSingleTapUp: arbi track stopped");
+            arbiTrack.getTargetNode().setVisible(true);
+        }
+
+        // If it's not tracking, start tracking and hide the target node.
+        else
+        {
+            arbiTrack.start();
+            Log.d(TAG, "onSingleTapUp: arbi track started");
+            arbiTrack.getTargetNode().setVisible(false);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
